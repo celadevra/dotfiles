79cfcc555440cc343e60ef903a1c70fda9669660
Haoyang Xu <snakehsu@gmail.com>
2013-05-14 19:38:37 +0800

    Add a space between linum and text

--- prelude-personal/look.el
+++ prelude-personal/look.el
@@ -3,6 +3,19 @@
 (disable-theme 'zenburn)
 (load-theme 'molokai)
 
+; Add space between linum and text
+; from http://www.emacswiki.org/emacs/LineNumbers#toc7
+(defvar my-linum-format-string "%4d")
+(add-hook 'linum-before-numbering-hook 'my-linum-get-format-string)
+(defun my-linum-get-format-string ()
+  (let* ((width (length (number-to-string
+                         (count-lines (point-min) (point-max)))))
+         (format (concat "%" (number-to-string width) "d ")))
+    (setq my-linum-format-string format)))
+(setq linum-format 'my-linum-format)
+(defun my-linum-format (line-number)
+  (propertize (format my-linum-format-string line-number) 'face 'linum))
+
 (provide 'look)
 
 ;; look.el ends here
