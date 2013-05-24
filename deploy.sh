872c91396b56a0353864f4dd534e088b850a9eca
Haoyang Xu <snakehsu@gmail.com>
2013-05-24 09:52:20 +0800

    Add commands to deploy

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,10 @@
 
 cp -p zshrc $HOME/.zshrc
 
+cp -p goobookrc $HOME/.goobookrc
+
+cp -p muttrc $HOME/.muttrc
+
 cp -p tmux.conf $HOME/.tmux.conf
 
 cp -p prelude-personal/*.el $HOME/.emacs.d/personal/
