3a611963d990157a129b5130f742aafede04fa1d
Haoyang Xu <snakehsu@gmail.com>
2013-07-25 11:49:27 +0800

    Add org settings

--- prelude-personal/behaviour.el
+++ prelude-personal/behaviour.el
@@ -6,3 +6,10 @@
 ; disable key-chord mode
 (key-chord-mode 0)
 (global-linum-mode 1)
+
+;; Customizing org-mode
+
+(add-hook 'org-mode-hook (lambda () (linum-mode 0)))
+(add-hook 'org-mode-hook (lambda () (whitespace-mode 0)))
+(setq org-mobile-directory "/ssh:dev.idenizen.net#2121:/home/snakehsu/mobileorg")
+(setq org-agenda-files '("~/org/office.org"))
