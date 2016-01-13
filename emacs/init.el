41d920e2e8c1d8e699c9741abcc21f7714c91ee8
Haoyang Xu <github@expoundite.net>
2016-01-13 22:49:19 +0800

    customize agenda views
    one for displaying done/cancelled tasks. the other for unscheduled
    tasks.

--- emacs/init.el
+++ emacs/init.el
@@ -14,6 +14,21 @@
     ("b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "f5eb916f6bd4e743206913e6f28051249de8ccfd070eae47b5bde31ee813d55f" "12b4427ae6e0eef8b870b450e59e75122d5080016a9061c9696959e50d578057" default)))
  '(haskell-tags-on-save t)
  '(magit-diff-options nil)
+ '(org-agenda-custom-commands
+   (quote
+    (("n" "Agenda and all TODOs"
+      ((agenda "" nil)
+       (alltodo "" nil))
+      nil)
+     ("x" "All DONE/CANCELLED tasks" todo "DONE|CANCELLED" nil)
+     ("u" "All unscheduled tasks" todo ""
+      ((org-agenda-skip-function
+	(lambda nil
+	  (org-agenda-skip-entry-if
+	   (quote scheduled)
+	   (quote deadline)
+	   (quote regexp)
+	   "]+>"))))))))
  '(org-agenda-files (quote ("~/codes/org/tasks.org")))
  '(org-bullets-bullet-list (quote ("⦿" "❥" "▼" "❖" "✫" "✻")))
  '(send-mail-function (quote smtpmail-send-it))
