21f8c806eeaaa6856b84e7b5881b0fc1d2de3e95
Haoyang Xu <haoyang@fastmail.com>
2016-08-27 16:24:40 +0800

    deps and org agenda settings

--- emacs/init.el
+++ emacs/init.el
@@ -11,3 +11,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(haskell-tags-on-save t)
+ '(pyim-dicts
+   (quote
+    ((:name "BigDict-01" :file "/Users/xhy/.emacs.d/pyim/dicts/pyim-bigdict.pyim.gz" :coding utf-8-unix :dict-type pinyin-dict)))))
