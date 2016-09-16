6b2e931733d546488b6ed889327d1b9459b33a9d
Haoyang Xu <Haoyang Xu>
2016-09-16 17:41:20 +0800

    bare bone config

--- emacs/init.el
+++ emacs/init.el
@@ -2,29 +2,3 @@
 (setq package-enable-at-startup nil)
 (require 'org)
 (org-babel-load-file "~/.emacs.d/Haoyang.org")
-
-(setq magit-last-seen-setup-instructions "1.4.0")
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(erc-email-userid "haoyang@expoundite.net")
- '(erc-nick "snakehsu")
- '(erc-nick-uniquifier "_")
- '(erc-port 9999)
- '(erc-server "23.83.236.195")
- '(erc-server-auto-reconnect t)
- '(erc-server-reconnect-timeout 30)
- '(haskell-tags-on-save t)
- '(org-bullets-bullet-list (quote ("◎" "⦿" "➢" "✣" "✫" "✲")))
- '(pyim-dicts
-   (quote
-    ((:name "BigDict-01" :file "/Users/xhy/.emacs.d/pyim/dicts/pyim-bigdict.pyim.gz" :coding utf-8-unix :dict-type pinyin-dict)))))
