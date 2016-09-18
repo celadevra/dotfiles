648480a68e486b7d2d36b868a6f5edd3d54ae1bc
Haoyang Xu <Haoyang Xu>
2016-09-18 11:31:57 +0800

    use-package based config

--- emacs/init.el
+++ emacs/init.el
@@ -2,3 +2,18 @@
 (setq package-enable-at-startup nil)
 (require 'org)
 (org-babel-load-file "~/.emacs.d/Haoyang.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (chinese-pyim-greatdict org-ref use-package org-plus-contrib org-bullets org key-chord ivy hydra f evil elfeed deft company-web company-ghci chinese-pyim ace-window)))
+ '(pyim-dicts nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
