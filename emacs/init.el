40de9966841b610c22907afad02f5c221538b0fc
Haoyang Xu <Haoyang Xu>
2016-10-27 14:55:52 +0800

    correct way to use proxy in elfeed

--- emacs/init.el
+++ emacs/init.el
@@ -7,9 +7,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(elfeed-curl-extra-arguments (quote ("-xsocks5h://127.0.0.1:1080")))
  '(package-selected-packages
    (quote
-    (darktooth-theme counsel chinese-pyim-greatdict use-package org-bullets org key-chord ivy hydra f evil elfeed deft company-web company-ghci chinese-pyim ace-window)))
+    (darktooth-theme counsel chinese-pyim-greatdict org-bullets org key-chord ivy hydra f evil elfeed deft company-web company-ghci chinese-pyim ace-window)))
  '(pyim-dicts nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
