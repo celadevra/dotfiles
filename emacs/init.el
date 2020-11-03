9763ee16801cc13872ce4c563f9ce7cabf4f5c32
徐栖 Xuqi <celadevra@users.noreply.github.com>
2020-11-03 22:57:58 +0800

    use straight.el

--- emacs/init.el
+++ emacs/init.el
@@ -1,5 +1,23 @@
-(package-initialize nil)
-(setq package-enable-at-startup nil)
+;; * Bootstraping Package system
+(server-start)
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+	"straight/repos/straight.el/bootstrap.el"
+	user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
+
 (require 'org)
 (org-babel-load-file "~/.emacs.d/Haoyang.org")
 (custom-set-variables
